58893cb17de4a44f3a3c8965c0d32aed1c72a73e
Shiwam Bhargava <shiwam17@gmail.com>
2021-12-16 00:15:10 +0530

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,34 @@ import React from 'react'
 import { Table, Container, Button } from 'react-bootstrap'
 
 const Home = () => {
-    return <div>
-        home
-    </div>
+    const pollList = ["who is going to win the contest", "which is the better js framework"]
+    return (
+        <Container>
+            <Table style={{ margin: "5vh" }} stripped bordered hover >
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>List of Poll</th>
+                        <th>Go to Poll</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {pollList.map((poll, id) => {
+                        return (
+                            <tr key={id}>
+                                <td>{id + 1}</td>
+                                <td>{poll}</td>
+                                <td>
+                                    <Button>Go to poll</Button>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+
+            </Table>
+        </Container>
+    )
 }
 
 export default Home
